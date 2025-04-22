4e8c8585a87915fbee40ba8281a40d49f7684dcf
Cristian Nadj <cnadj@tdsi.com>
2025-04-22 10:25:08 -0300

    Modifying the constructor to get a better PO structure

--- cypress/pages/SortableTablePage.js
+++ cypress/pages/SortableTablePage.js
@@ -1,34 +1,44 @@
+
 class SortableTablePage {
-    visit() {
-      cy.visit('/tables');
-    }
-  
-    getTable() {
-      return cy.get('#table1');
-    }
-  
-    getColumnHeaders() {
-      return this.getTable().find('thead th');
-    }
-  
-    getRows() {
-      return this.getTable().find('tbody tr');
-    }
-  
-    clickColumnHeader(headerText) {
-      this.getColumnHeaders()
-        .contains(headerText)
-        .click();
-    }
-  
-    getCellText(rowIndex, columnIndex) {
-      return this.getRows()
-        .eq(rowIndex)
-        .find('td')
-        .eq(columnIndex)
-        .invoke('text');
-    }
+  constructor() {
+    this.url = '/tables';
+    this.selectors = {
+      table: '#table1',
+      columnHeaders: 'thead th',
+      rows: 'tbody tr',
+      rowCells: 'td',
+    };
   }
-  
-  export default new SortableTablePage();
-  
+
+  visit() {
+    cy.visit(this.url);
+  }
+
+  getTable() {
+    return cy.get(this.selectors.table);
+  }
+
+  getColumnHeaders() {
+    return this.getTable().find(this.selectors.columnHeaders);
+  }
+
+  getRows() {
+    return this.getTable().find(this.selectors.rows);
+  }
+
+  clickColumnHeader(headerText) {
+    this.getColumnHeaders()
+      .contains(headerText)
+      .click();
+  }
+
+  getCellText(rowIndex, columnIndex) {
+    return this.getRows()
+      .eq(rowIndex)
+      .find(this.selectors.rowCells)
+      .eq(columnIndex)
+      .invoke('text');
+  }
+}
+
+export default new SortableTablePage();
